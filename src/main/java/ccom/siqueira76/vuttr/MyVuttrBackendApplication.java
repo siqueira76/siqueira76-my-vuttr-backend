1c946ba2e753252252a28f94358f141916930677
José Carlos Siqueira <jose.a.siqueira@capgemini.com>
2020-07-18 13:45:06 -0300

    v 0.0.1 - First commit

---
+++ src/main/java/ccom/siqueira76/vuttr/MyVuttrBackendApplication.java
@@ -0,0 +1,13 @@
+package ccom.siqueira76.vuttr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVuttrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVuttrBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ccom/siqueira76/vuttr/MyVuttrBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package ccom.siqueira76.vuttr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyVuttrBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
